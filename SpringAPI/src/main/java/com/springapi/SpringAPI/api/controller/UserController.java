e5ce70bf36b4b604476b91786134908dfbad144a
Kyle Khai Tran <kylekhai04@gmail.com>
2023-08-01 18:59:38 -0400

    Add user examples in userList + inject service to controller

--- SpringAPI/src/main/java/com/springapi/SpringAPI/api/controller/UserController.java
+++ SpringAPI/src/main/java/com/springapi/SpringAPI/api/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.springapi.SpringAPI.api.controller;
 
+import com.springapi.SpringAPI.api.model.User;
+import com.springapi.SpringAPI.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
     
+    private UserService userService;
+
+    //Injects userservice service into the controller
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    //Allows for GET calls to API setup
+    @GetMapping 
+    public User getUser(@RequestParam Integer id) {
+        return userService.getUser(id);
+    }
 }
